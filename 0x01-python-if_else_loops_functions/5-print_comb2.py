c6ddc2efa93783b13efd1e0b8705c6118a7a8845
Mathenge-Alex <arxmaina@gmail.com>
2024-03-13 03:38:06 +0300

    fix: print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i != 99:
-        print(f"{i:02d}", end=", ")
+for j in range(100):
+    if j < 99:
+        print('{:02d}, '.format(j), end='')
     else:
-        print(f"{i:02d}")
+        print(j)
