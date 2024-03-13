60620a5fddf78e5b9d5a53ddcad260371b1647e4
Mathenge-Alex <arxmaina@gmail.com>
2024-03-13 03:39:55 +0300

    fix: print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i < 8 else "\n")
+for j in range(10):
+    for k in range(10):
+        if (j != k and j < k) and j < 9:
+            if (j == 8 and k == 9):
+                print('{0}{1}'.format(j, k))
+            else:
+                print('{0}{1}, '.format(j, k), end='')
