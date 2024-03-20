f9f8ea7dfc863dc362c34dea1aa66f058a8ea2b7
Mathenge-Alex <arxmaina@gmail.com>
2024-03-20 13:03:09 +0300

    feat: Weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return 0
 
     result = sum([multiply(j[0], j[1]) for j in my_list]) / sum(j[1] for j in my_list)
-    return "Average: {:0.2f}".format(result)
+    return "Average: {}".format(result)
 
 def multiply(j, k):
     return j * k
