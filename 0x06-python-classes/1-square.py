bc9fd8e97061031b7413e983bca40497e5d988db
Mathenge-Alex <arxmaina@gmail.com>
2024-04-04 00:49:47 +0300

    Feat: Python Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Definition of a class square"""
+
+
+class Square:
+    """This is a square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
