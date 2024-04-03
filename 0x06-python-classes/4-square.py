cb6589933d117448ff28bc628d18387f3a15fa7e
Mathenge-Alex <arxmaina@gmail.com>
2024-04-04 00:50:02 +0300

    Feat: Python Classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Definition of a class square"""
+
+
+class Square:
+    """This is a square."""
+
+    def __init__(self, size=0):
+        """Initializing a new Square.
+        Args:
+            size (int): Size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the current area of the square."""
+        return (self.__size * self.__size)
