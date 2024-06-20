8e17669411475d6d1ad690e6e696c0be5fb79464
Mathenge-Alex <arxmaina@gmail.com>
2024-06-20 22:18:05 +0300

    JavaScript warm up - 0x0E-SQL_more_queries/0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 --List all privileges MySQL Users
--- user_0d_1 and user_0d_2 on your server
+-- user_0d_1 and user_0d_2 on server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
